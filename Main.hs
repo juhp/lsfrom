8c878e6638e61f7fb12440fe2fc42d5c1cd5d8c8
Jens Petersen <petersen@redhat.com>
2021-01-28 13:22:30 +0800

    use case

--- Main.hs
+++ Main.hs
@@ -8,14 +8,12 @@ import System.Environment
 main :: IO ()
 main = do
   args <- getArgs
-  if length args /= 1
-    then error "Usage: lsfrom [FILE]"
-    else do
-    let start = head args
-    files <- sort <$> (getCurrentDirectory >>= listDirectory)
-    mapM_ (printFrom start) files
+  case args of
+    [start] -> do
+      files <- sort <$> (getCurrentDirectory >>= listDirectory)
+      mapM_ (printFrom start) files
+    _ -> error "Usage: lsfrom [FILE]"
 
 printFrom :: String -> String -> IO ()
 printFrom start f =
   when (f >= start) $ putStrLn f
-
