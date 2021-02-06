a980c4d0777b399fe53abfb7c0fd1d8b9a9deea7
Jens Petersen <petersen@redhat.com>
2021-02-06 14:22:27 +0800

    add simple testsuite

---
+++ test/tests.hs
@@ -0,0 +1,24 @@
+import Control.Monad
+import SimpleCmd
+import System.Directory
+
+lsfrom :: (FilePath, String, [String]) -> IO ()
+lsfrom (dir, arg,expect) = do
+  out <- withCurrentDirectory dir $
+         cmdLines "lsfrom" [arg]
+  unless (out == expect) $ do
+    cmdN "lsfrom" [arg]
+    putStrLn $ "returned> " ++ show out
+    putStrLn $ "expected> " ++ show expect
+    error' "failed"
+
+tests :: [(FilePath, String,[String])]
+tests =
+  [ ("test/files", "B", ["B","C"])
+  , ("test", "files/B/", ["files/C"])
+  ]
+
+main :: IO ()
+main = do
+  mapM_ lsfrom tests
+  putStrLn $ show (length tests) ++ " tests run"
